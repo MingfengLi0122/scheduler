00a59e65986c3357ef43cec85c576f2ed799b31b
MingfengLi0122 <lmf950122@gmail.com>
2021-07-05 22:08:01 -0700

    Create button component by using react

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,30 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   // approach one: use if condition
+
+   // let buttonClass = "button";
+   
+   // if (props.confirm) {
+   //    buttonClass += " button--confirm";
+   // }
+   // if (props.danger) {
+   //    buttonClass += " button--danger"
+   // }
+
+   // approach two: use classnames
+   const classes = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger,
+   });
+   
+   return <button 
+            className={classes}
+            onClick={props.onClick}
+            disabled={props.disabled}
+         >
+            { props.children }
+         </button>;
 }
