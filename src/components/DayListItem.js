e481fd0f963d0322604a25b151257f524b34b113
MingfengLi0122 <lmf950122@gmail.com>
2021-07-05 22:13:08 -0700

    Create daylistitem component by using react

---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
+
+export default function DayListItem(props) {
+  // console.log(props);
+  const classes = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  const fomatSpots = function() {
+    if (!props.spots) {
+      return "no spots remaining";
+    }
+
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    
+    return props.spots + " spots remaining";
+  }
+  
+  return (
+    <li className={classes} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{fomatSpots()}</h3>
+    </li>
+  );
+}
