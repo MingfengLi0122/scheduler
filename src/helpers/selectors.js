599ba3313d26712f31344c8db6837da204947001
MingfengLi0122 <lmf950122@gmail.com>
2021-07-08 20:04:28 -0700

    Create getInterviewersForDay helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,33 @@
-export default function getAppointmentsForDay(state, day) {
+function getAppointmentsForDay(state, day) {
   let filteredResult = [];
   const filteredDay= state.days.filter(dayInfo => dayInfo.name === day);
 
-  if(!filteredDay[0]) return filteredResult;
-  for (const interviewerId of filteredDay[0].appointments) {
-    filteredResult.push(state.appointments[interviewerId])
+  if (!filteredDay.length) return filteredResult;
+  for (const appointmentId of filteredDay[0].appointments) {
+    filteredResult.push(state.appointments[appointmentId]);
   }
 
   return filteredResult;
 }
+
+function getInterview(state, interview) {
+  if(!interview) return null;
+  const interviewerId = interview.interviewer;
+  let interviewer = state.interviewers[interviewerId ];
+ 
+  return {...interview,  interviewer}
+};
+
+function getInterviewersForDay(state, day) {
+  let filteredResult = [];
+  const filteredDay = state.days.filter(dayInfo => dayInfo.name === day);
+
+  if (!filteredDay.length) return filteredResult;
+  for (const interviewerId of filteredDay[0].interviewers) {
+    filteredResult.push(state.interviewers[interviewerId]);
+  }
+
+  return filteredResult;
+};
+
+export { getAppointmentsForDay, getInterview, getInterviewersForDay }
